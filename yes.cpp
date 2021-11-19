b90e1ce9a1d94a0cacd86ebce0610e99df9dffd4
Boris2006 <92259287+Boris2006@users.noreply.github.com>
2021-11-19 10:43:21 +0300

    Update yes.cpp

--- yes.cpp
+++ yes.cpp
@@ -1,1 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+string perevod(string a){
+    string a = "", b = "";
+    long long c  = 0;
+    for (a; a[c] != "."; i++){
+        if 
+    }
 
+}

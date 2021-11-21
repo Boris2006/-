ff3383d9b502844637c5546f7149ec83957f5d7b
Boris2006 <92259287+Boris2006@users.noreply.github.com>
2021-11-21 15:20:14 +0300

    Update yes.cpp

--- yes.cpp
+++ yes.cpp
@@ -1,11 +1,87 @@
-#include <iostream>
 #include <string>
+#include <iostream>
 using namespace std;
-string perevod(string a){
-    string a = "", b = "";
-    long long c  = 0;
-    for (a; a[c] != "."; i++){
-        if 
+long long degree(long long c){
+    long long a = 1;
+    while(c != 0){
+        a = a * 10;
+        c--;
     }
+    return a;
+}
+string sign(string a) {
+    string c;
+    if (a[0] == '-'){
+        c += "1";
+    }else{
+        c += "0";}
+    return c;}
+string clear(string a){
+    string c;
+    for (int i = 1; a[i] != '\0'; i++){
+        c += a[i];}
+    return c;
+}
+long long len(string a){
+  int b = 0;
+    while (a[b] != '\0'){
+        b++;}
+    return b;
+}
 
+string translation(string a){
+    string b;
+    long long c = 0;
+    for (int i = 0; a[i] != '\0'; i++){}
+    return "0";
+}
+long long  translation2(string a){
+    string b;
+    long long c, d = 0;
+    for (long long i = 0; a[i] != '.'; i++){
+        b += a[i];}
+    c = len(b);
+    for (long long i = 0; b[i] != '\0'; i++){
+        if (b[i] == '0'){
+            d += 0 * degree(c);
+            c--;}
+        if (b[i] == '1'){
+            d += 1 * degree(c);
+            c--;}
+        if (b[i] == '2'){
+            d += 2  * degree(c);
+            c--;}
+        if (b[i] == '3'){
+            d += 3 * degree(c);
+            c--;}
+        if (b[i] == '4'){
+            d += 4 * degree(c);
+            c--;}
+        if (b[i] == '5'){
+            d += 5 * degree(c);
+            c--;}
+        if (b[i] == '6'){
+            d += 6 * degree(c);
+            c--;}
+        if (b[i] == '7'){
+            d += 7 * degree(c);
+            c--;}
+        if (b[i] == '8'){
+            d += 8 * degree(c);
+            c--;}
+        if (b[i] == '9'){
+            d += 9 * degree(c);
+            c--;}
+    }
+    d = d / 10;
+    return d;
+}
+int main(){
+    string a, b, c;
+    long long p;
+    cin >> a;
+    b += sign(a);
+    a = clear(a);
+    p = translation2(a);
+    cout << p;  
 }
